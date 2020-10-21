041b12443086eaf61e651e248b23e4c5bd31c5c5
nalmt <nabil.lamrabet@pm.me>
2020-10-21 11:13:24 +0200

    matrix done

---
+++ main.py
@@ -0,0 +1,64 @@
+# TP2 SMA 
+# 21/10/20
+import numpy as np
+from enum import Enum
+import random
+
+i = 1
+number_of_agents = 20
+k_plus = 0.1
+k = 0.3
+na = 200
+nb = 200
+
+matrix = np.full((50, 50), '0')
+
+agents_list = []
+
+def fill_with(number_of_objects, object_type):
+    for machin in range(number_of_objects):
+        l = random.randint(0, 49)
+        c = random.randint(0, 49)
+
+        while matrix[l][c] != '0':
+            l = random.randint(0, 49)
+            c = random.randint(0, 49)
+        
+        matrix[l][c] = object_type
+
+def fill_agent(number_of_objects):
+    for machin in range(number_of_objects):
+        l = random.randint(0, 49)
+        c = random.randint(0, 49)
+
+        while matrix[l][c] != '0':
+            l = random.randint(0, 49)
+            c = random.randint(0, 49)
+
+        agents_list.append(Agent(l, c))
+        matrix[l][c] = 'X'
+
+
+
+class Agent:
+
+    t_size = 10
+    t = []
+    def __init__(self, l, c):
+        self.l = l
+        self.c = c
+
+
+def  main():
+    fill_with(na, 'A')
+    fill_with(nb, 'B')
+    fill_agent(number_of_agents)
+    print(matrix)
+    print(agents_list)
+
+
+#def scheduler():
+
+
+if __name__ == "__main__":
+    main()
